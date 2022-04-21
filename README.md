bb19b188c6732d414b99fa088a79c605769cd787
LilJawa <dajawaman@outlook.com>
2022-04-22 05:20:49 +1000

    Pre-Update

---
+++ README.md
@@ -0,0 +1,43 @@
+# Ethermon
+
+A Multiplayer Open-Source Pokemon Game Built on Top of Ethercrashio and Ether-Dice.io Games.
+
+## Description
+
+Designed to be injected into Ethercrash.io/play and Ether-Dice.io/play, to add a layer for playing other players in a multiplayer Pokemon Style Game.
+
+## Getting Started
+
+### Dependencies
+
+* A Browser on a Laptop or Computer with Console Access on said Browser.
+
+### Executing program
+
+* To play, copy the script [here](https://github.com/LilJawa/Ethermon/blob/main/index.js) into the console or in the Script Boxes provided.
+
+### Contribute
+
+* To contribute, create a well structured Pull Request.
+
+
+## Authors
+
+Lil Jawa  
+[LilJawa](https://github.com/LilJawa)
+
+## Version History
+
+* 1.0.2
+    * Various Additions to allow for Remote Execution
+    * See [commit change]()
+    * Initial Creation
+
+## License
+
+This project is licensed under the [Creative Commons Zero v1.0 Universal] License - see the LICENSE.md file for details
+
+## Acknowledgments
+
+Inspirations, code snippets, open-source credits.
+* [DaFont Pokemon Font](https://www.dafont.com/pokemon.font)
---
+++ Scripts/Inject.js
@@ -0,0 +1,44 @@
+var Config = {
+	Title: 'Ethermon',
+	Version: 1.01
+} 
+var GUI = {
+	DOM: document,
+	Canvas: null,
+	Context: null,
+	TitleFont: null
+}
+function StartEthermon(){
+	GUI.TitleFont = TitleFont;
+    	CreateCanvas();
+	GUI.Canvas = document.getElementById('EthermonCanvas');
+	GUI.Context = GUI.Canvas.getContext('2d');
+	Draw();
+	window.scrollTo(0,-100);
+}
+function CreateCanvas(){
+	if (CanvasExists()) GUI.Canvas.remove();
+	GUI.Canvas = GUI.DOM.createElement(`canvas`);
+	GUI.Canvas.id = 'EthermonCanvas';
+	GUI.Canvas.height = 200;
+	GUI.Canvas.width = window.innerWidth;
+	GUI.DOM.getElementById('play-wrapper').prepend(GUI.Canvas);
+}
+function TitleFont(){
+	return new FontFace('Font name', 'url(/path/to/font.ttf)');
+}
+function CanvasExists(){
+	GUI.Canvas = GUI.DOM.getElementById("EthermonCanvas");
+	return GUI.Canvas != undefined;
+}
+function Draw() {
+        GUI.Context.clearRect(0, 0, GUI.Canvas.width, GUI.Canvas.height);
+        DrawInfo();
+        requestAnimationFrame(Draw);
+}
+function DrawInfo() {
+        GUI.Context.font = "18px Arial";
+        GUI.Context.fillStyle = "#0095DD";
+        GUI.Context.fillText(Config.Title +" v"+Config.Version, 25, 60);
+}
+function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
