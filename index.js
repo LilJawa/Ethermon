86c0387baf0817d5e75c8f15d48bb96d0cba9a48
LilJawa <dajawaman@outlook.com>
2022-04-22 04:47:17 +1000

    Test Remote Upload, Set Repo Public

--- index.js
+++ index.js
@@ -1,46 +1,8 @@
-var Config = {
-	Title: 'Ethermon'
-} 
-var GUI = {
-	DOM: document,
-	Canvas: null,
-	Context: null
-}
+Import();
 Init();
-//Import();
-function Init(){
-    	GUI.Canvas = GUI.DOM.getElementById("canvasId");
-	if (GUI.Canvas != undefined) GUI.Canvas.remove();
-	AddCanvas();
-	GUI.Canvas = document.getElementById('canvasId');
-	GUI.Context = GUI.Canvas.getContext('2d');
-	Draw();
-}
-function AddCanvas(){
-GUI.Canvas = GUI.DOM.createElement(`canvas`);
-GUI.Canvas.id = 'canvasId';
-GUI.Canvas.height = 200;
-GUI.Canvas.width = window.innerWidth;
-GUI.DOM.getElementById('play-wrapper').prepend(GUI.Canvas);
-}
 function Import(){
 	var script = document.createElement('script');
  	script.type = 'text/javascript';
  	script.src = 'https://raw.githubusercontent.com/LilJawa/Ethermon/main/import.js';    
-  	GUI.DOM.head.appendChild(script);
+  	document.head.appendChild(script);
 }
-
-function Draw() {
-        GUI.Context.clearRect(0, 0, GUI.Canvas.width, GUI.Canvas.height);
-        DrawInfo();
-        requestAnimationFrame(Draw);
-}
- function DrawInfo() {
-        GUI.Context.font = "18px Arial";
-        GUI.Context.fillStyle = "#0095DD";
-        GUI.Context.fillText(Config.Title, (GUI.Canvas.width/4)-40, 60);
-}
-
-
-window.scrollTo(0,-100);
-const sleep = ms => new Promise(r => setTimeout(r, ms));
