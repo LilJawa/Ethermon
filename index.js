9f24623e2f3fb50243244e552313560b2841cd58
LilJawa <dajawaman@outlook.com>
2022-04-22 04:56:30 +1000

    Add Import & Execution of Script from Github CDN

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 Import();
-Init();
 function Import(){
 	var script = document.createElement('script');
  	script.type = 'text/javascript';
- 	script.src = 'https://raw.githubusercontent.com/LilJawa/Ethermon/main/import.js';    
+ 	script.src = 'https://cdn.rawgit.com/LilJawa/Ethermon/main/import.js';   
+ 	script.onload = function() {
+  	Init();
+	}; 
   	document.head.appendChild(script);
 }
