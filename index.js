51261dac12ee2c376410066a09e417da5032c3c9
LilJawa <dajawaman@outlook.com>
2022-04-22 04:37:27 +1000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var Config = {
+	Title: 'Ethermon'
+} 
+var GUI = {
+	DOM: document,
+	Canvas: document.createElement(`canvas`),
+	Context: null
+}
+Init();
+//Import();
+function Init(){
+    	var GUI.Canvas = GUI.DOM.getElementById("canvasId");
+	if (GUI.Canvas != undefined) GUI.Canvas.remove();
+	AddCanvas();
+	GUI.Canvas = document.getElementById('canvasId');
+	GUI.Context = GUI.Canvas.getContext('2d');
+	Draw();
+}
+function AddCanvas(){
+GUI.Canvas.id = 'canvasId';
+GUI.Canvas.height = 200;
+GUI.Canvas.width = window.innerWidth;
+GUI.DOM.getElementById('play-wrapper').prepend(GUI.Canvas);
+}
+function Import(){
+	var script = document.createElement('script');
+ 	script.type = 'text/javascript';
+ 	script.src = 'https://github.com/LilJawa/Ethermon/import.js';    
+  	GUI.DOM.head.appendChild(script);
+}
+
+function Draw() {
+        GUI.Context.clearRect(0, 0, GUI.Canvas.width, GUI.Canvas.height);
+        DrawInfo();
+        requestAnimationFrame(Draw);
+}
+ function DrawInfo() {
+        GUI.Context.font = "18px Arial";
+        GUI.Context.fillStyle = "#0095DD";
+        GUI.Context.fillText(Config.Title, (GUI.Canvas.width/4)-40, 60);
+}
+
+
+window.scrollTo(0,-100);
+const sleep = ms => new Promise(r => setTimeout(r, ms));
